85445e185503798295103c23cff259c3c380c95d
Casimir COMPAORE <casimircompaore@MacBook-Pro-de-Casimir.local>
2021-09-20 08:02:34 +0200

    reached iteration

--- hello.go
+++ hello.go
@@ -2,15 +2,36 @@ package main
 
 import "fmt"
 
-const englishPrefixHello = "Hello, "
+const spanish = "Spanish"
+const french = "French"
 
-func Hello(name string) string {
+const englishPrefixHello = "Hello, "
+const spanishPrefixHello = "Hola, "
+const frenchPrefixHello = "Bonjour, "
+
+func Hello(name string, langage string) string {
+
 	if name == "" {
 		name = "World"
 	}
-	return englishPrefixHello + name
+
+	return greetingPrefix(langage) + name
+}
+
+func greetingPrefix(langage string) (prefix string) {
+
+	prefix = englishPrefixHello
+
+	switch langage {
+	case spanish:
+		prefix = spanishPrefixHello
+	case french:
+		prefix = frenchPrefixHello
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "French"))
 }
